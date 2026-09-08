5746f05158c806e640dfe01ca0b39d97f185120f
Aaron Fuenzalida <afuenzalida243@gmail.com>
2026-09-08 16:56:57 -0300

    feat: add auction and category services

--- subastaYa-backend/src/SubastaYa.Application/DependencyInjection.cs
+++ subastaYa-backend/src/SubastaYa.Application/DependencyInjection.cs
@@ -3,6 +3,10 @@ using SubastaYa.Application.Auth.Interfaces;
 using SubastaYa.Application.Auth.Services;
 using SubastaYa.Application.Wallets.Interfaces;
 using SubastaYa.Application.Wallets.Services;
+using SubastaYa.Application.Categories.Interfaces;
+using SubastaYa.Application.Categories.Services;
+using SubastaYa.Application.Auctions.Interfaces;
+using SubastaYa.Application.Auctions.Services;
 
 namespace SubastaYa.Application;
 
@@ -12,6 +16,8 @@ public static class DependencyInjection
     {
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IWalletService, WalletService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IAuctionService, AuctionService>();
         return services;
     }
 }
